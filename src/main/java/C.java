868269803da28a9d0c0411e2e1460d98a0ea8a2f
dzp <1582982151@qq.com>
2022-11-02 16:02:01 +0800

    hello

---
+++ src/main/java/C.java
@@ -0,0 +1,11 @@
+
+import java.util.List;
+
+public class C {
+    public static void main(String []args) {
+        A a =new A();
+        B b=new B();
+
+        System.out.println("hello");
+    }
+}
